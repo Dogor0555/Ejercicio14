0be0c244cc95a0b937fd4f732ef4ffad4d804ac6
Dogor0555 <74686946+Dogor0555@users.noreply.github.com>
2025-02-15 06:20:51 -0600

    Ejercicio 14

--- Ejercicio14/Program.cs
+++ Ejercicio14/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+Console.Write("Ingrese un número: ");
+int numero = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Tabla de multiplicar de {numero}:");
+
+for (int i = 1; i <= 10; i++)
+{
+    int resultado = numero * i;
+    Console.WriteLine($"{numero} x {i} = {resultado}");
+}
